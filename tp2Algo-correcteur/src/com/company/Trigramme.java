2c36a3455c2af0ff6cdcdc6be2f69e6116cfdc19
Velexi <faycal.henis@yahoo.fr>
2019-10-30 21:53:02 +0100

    Trigramme : Affichage des trigrammes d'un mot, création des listes de trigrammes

--- tp2Algo-correcteur/src/com/company/Trigramme.java
+++ tp2Algo-correcteur/src/com/company/Trigramme.java
@@ -1,13 +1,16 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.List;
+import java.util.*;
 
 public class Trigramme {
     private Hashtable<String, List<String>> trigramme = new Hashtable<>();
+    private List<String> L = new ArrayList<>() ;
 
+
+    public Trigramme(String mot){
+
+        createTrigramme(mot);
+    }
     public void createTrigramme(String mot){
         mot = "<"+mot+">";
         if(trigramme.containsKey(mot)) return;
@@ -17,5 +20,55 @@ public class Trigramme {
         }
         trigramme.put(mot,aaa);
     }
+
+    public void printTrigramme(String mot){
+        mot = "<"+mot+">";
+       List<String> t = getTrigrammeList(mot);
+       String output = "";
+       for(int i=0; i<t.size(); i++){
+           output = output + t.get(i)+", ";
+       }
+        output = output.substring(0,output.length()-2);
+        System.out.println(output);
+    }
+
+    public  void createL(String mot,String w){
+        Trigramme u = new Trigramme(w);
+        w="<"+w+">";
+        mot="<"+mot+">";
+        List<String> list = u.getTrigrammeList(w);
+        List<String> trigrammes = this.getTrigrammeList(mot);
+        for(String trig : list){
+            if(trigrammes.contains(trig)){
+                L.add(w.replace(">","").replace("<",""));
+                break;
+            }
+            else continue;
+        }
+    }
+
+    public int occurenceInList(List<String> mots, String mot){
+        int occ =0;
+        if(!mots.contains(mot)) return 0;
+        else {
+            for(int i=0; i<mots.size(); i++){
+                if (mot.equals(mots.get(i))){
+                    occ++;
+                }
+            }
+        }
+
+        return occ;
+
+    }
+
+    public List<String> getTrigrammeList(String mot){
+        return trigramme.get(mot);
+    }
+
+    public List<String> getL() {
+        return L;
+    }
+
 
 }
