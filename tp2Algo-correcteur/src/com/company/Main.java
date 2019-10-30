a6c765b9f1eced09e3ccab38c0caab80129f0c02
Velexi <faycal.henis@yahoo.fr>
2019-10-30 21:51:14 +0100

    Quelques tests d'executions

--- tp2Algo-correcteur/src/com/company/Main.java
+++ tp2Algo-correcteur/src/com/company/Main.java
@@ -9,14 +9,39 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Dictionnaire d = new Dictionnaire("tp2Algo-correcteur/src/com/company/dico.txt");
-        Set<List<String>> set = Dictionnaire.getDictionnaire().keySet();
+        Set<List<String>> set = d.getDictionnaire().keySet();
         int lignes = 0;
         for (List<String> quelquechose : set) {
-            for (String string : Dictionnaire.getDictionnaire().get(quelquechose)) {
+            for (String string : d.getDictionnaire().get(quelquechose)) {
                 lignes++;
                 System.out.println(string);
                 System.out.println(lignes);
             }
         }
+
+        long starTime = System.nanoTime();
+        System.out.println(d.contains("anarquer"));
+        long endTime = System.nanoTime();
+
+        long elapsedTime = endTime - starTime;
+        System.out.println("Execution time in milliseconds :" + elapsedTime/100000 +" ms");
+        System.out.println();
+
+        System.out.println("=========Test des trigrammes===================");
+        System.out.println();
+
+
+        String mot = "Allier";
+        String w = "Alleur";
+        Trigramme t = new Trigramme(mot);
+        t.printTrigramme(mot);
+
+        t.createL(mot,w);
+        List<String> test = t.getL();
+        for(String k : test){
+            System.out.println(k);
+        }
+        System.out.println(t.occurenceInList(test,w));
     }
-}
+    }
+
