acf4695efd266cebc88b50468a9170017f95eb38
Velexi <faycal.henis@yahoo.fr>
2019-10-31 14:06:28 +0100

    Calcul des 5 mots les plus proches pour un mot mal orthographié donné. Reste à faire : optimisation

--- tp2Algo-correcteur/src/com/company/Main.java
+++ tp2Algo-correcteur/src/com/company/Main.java
@@ -1,9 +1,7 @@
 package com.company;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static int occurenceInList(List<String> mots, String mot){
@@ -32,13 +30,13 @@ public class Main {
             }
         }*/
 
-
-        String word = "util";
+        String word = "renomé";
         if (d.contains(word)) {
+            System.out.println("Le mot "+word+" est bien orthographié");
             System.exit(-1);
         }
         else{
-            System.out.println("le mot est mal orthographié");
+            System.out.println("le mot " + word+" est mal orthographié choisissez parmis ces mots :");
             Trigramme t = new Trigramme(word);
             List<String> tList = t.getTrigrammeList("<"+word+">");
             String firstT = tList.get(0);
@@ -56,15 +54,30 @@ public class Main {
                 }
             }
 
-            /*for(String string : wclose){
-                System.out.println(string);
-            }*/
+            Map<Integer, List<String>> motsfinaux = new HashMap<>();
             for(String string : wclose){
-                if(Levenstein.computeDistance(string,word)<3) System.out.println(string);
-
+                int dist = Levenstein.computeDistance(string,word);
+                if(!motsfinaux.containsKey(dist)){
+                    List<String> list = new ArrayList<>();
+                    list.add(string);
+                    motsfinaux.put(dist,list);
+                } else {
+                    List<String> list = new ArrayList<>();
+                    list.add(string);
+                    for (String x :list)
+                        if(!motsfinaux.get(dist).contains(x)) motsfinaux.get(dist).add(x);
+                }
+            }
+            Set<Integer> clefs = motsfinaux.keySet();
+            List<String> spellchecked = new LinkedList<>();
+            for(Integer integer: clefs){
+              spellchecked.addAll(motsfinaux.get(integer));
+            }
+            for(int cpt = 0; cpt <5; cpt++){
+                System.out.print(spellchecked.get(cpt)+" ");
             }
 
-            //System.out.println(occurenceInList(motsTrig,"outile"));
+
         }
 
 
